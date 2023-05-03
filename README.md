d4907d03103defc0193570b759aa49d2840fa293
Dasappanb <100546828+Dasappanb@users.noreply.github.com>
2023-05-03 15:10:42 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# anuj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/sveltejs-kit-template-default-msnhtj)
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,38 @@
+
+<html>
+<head>
+	<title>Attendance Calculator</title>
+</head>
+<body>
+	<h1>Attendance Calculator</h1>
+	<form>
+		<label for="attended">Number of classes attended:</label>
+		<input type="number" id="attended" name="attended" required>
+		<br>
+		<label for="total">Total number of classes:</label>
+		<input type="number" id="total" name="total" required>
+		<br>
+		<input type="button" value="Calculate" onclick="calculateAttendance()">
+		<br>
+		<label for="result">Attendance percentage:</label>
+		<input type="text" id="result" name="result" readonly>
+	</form>
+
+	
+</body>
+</html>
+
+<script>
+		function calculateAttendance() {
+			var attended = document.getElementById("attended").value;
+			var total = document.getElementById("total").value;
+
+			var percentage = (attended / total) * 100;
+      if(percentage<65)
+      {
+        alert("sem out")
+      }
+
+			document.getElementById("result").value = percentage.toFixed(2) + "%";
+		}
+	</script>
